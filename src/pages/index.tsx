7620d4741f4f0dbc3c367254424b627ea66f8c51
hota1024 <hotalog@hota1024.com>
2021-03-02 14:38:54 +0900

    :tada: init

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+/**
+ * HomePage component.
+ */
+export const HomePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>home - motimoti-ui</title>
+      </Head>
+
+      <div>motimoti-ui</div>
+    </>
+  )
+}
+
+export default HomePage
