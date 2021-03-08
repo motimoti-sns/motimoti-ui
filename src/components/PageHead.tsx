81657c4d1d931d4110cbde6ed0f85dbdb2035e4a
hota1024 <hotalog@hota1024.com>
2021-03-08 17:27:24 +0900

    🏷️ add PageHead component

---
+++ src/components/PageHead.tsx
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+import { useMetaContext } from '../contexts/MetaContext'
+
+/**
+ * PageHeadProps type.
+ */
+export type PageHeadProps = {
+  /**
+   * page title.
+   */
+  title?: string
+
+  /**
+   * page description.
+   */
+  description?: string
+}
+
+/**
+ * PageHead component.
+ */
+export const PageHead: React.FC<PageHeadProps> = (props) => {
+  const meta = useMetaContext()
+
+  return (
+    <>
+      <Head>
+        <title>{props.title ?? meta.catchphrase} / motimoti</title>
+        <meta
+          name="description"
+          content={props.description ?? meta.catchphrase}
+        />
+      </Head>
+    </>
+  )
+}
