f58fd6e0beff9c19ee4e878cc6cd78270950d927
hota1024 <hotalog@hota1024.com>
2021-03-13 01:10:29 +0900

    💄 set color

--- src/themes/main.ts
+++ src/themes/main.ts
@@ -1,3 +1,14 @@
 import { createMuiTheme } from '@material-ui/core'
 
-export const mainTheme = createMuiTheme({})
+export const mainTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#388E3C',
+      light: '#388E3C',
+    },
+    secondary: {
+      main: '#F9A825',
+      light: '#F9A825',
+    },
+  },
+})
