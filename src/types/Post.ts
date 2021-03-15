8a4e5ad85d450f6dcb4b02e3a69c58e39fdc321b
hota1024 <hotalog@hota1024.com>
2021-03-15 21:25:53 +0900

    🏷️ Add Post

---
+++ src/types/Post.ts
@@ -0,0 +1,24 @@
+/**
+ * Post type.
+ */
+export type Post = {
+  /**
+   * user id.
+   */
+  user_id: number
+
+  /**
+   * post id.
+   */
+  post_id: number
+
+  /**
+   * text body.
+   */
+  text_body: string
+
+  /**
+   * timestamp.
+   */
+  timestamp: string
+}
